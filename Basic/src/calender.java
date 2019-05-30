9015a192e8d4bdf6e58e6b553b7bc0c05116c2e6
UditGoyal <goyal.udit93@gmail.com>
2019-05-30 11:42:05 +0530

    new demo project

---
+++ Basic/src/calender.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class calender {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "F://chromedriver_win32//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.hotelbids.com/");
+		String strYear = "September 2019";
+		driver.findElement(By.name("start")).click();
+		while(!driver.findElement(By.className("datepicker-switch")).getText().equalsIgnoreCase(strYear))
+		{
+			driver.findElement(By.className("next")).click();
+		}
+		driver.findElement(By.xpath("//td[text()='19']")).click();
+	}
+
+}
